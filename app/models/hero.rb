459e873d03fa361943e8f2374155a3ac17def85a
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-28 01:30:19 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,11 +1,11 @@
 class Hero
-  attr_accessor :name, :power, :biography
+  attr_accessor :name, :power, :bio
   HEROES = []
 
   def initialize(params)
     @name = params[:name]
-    @power = params[:power]
-    @biography = params[:biography]
+    @power = params[:power]bio
+    @bio = params[:bio]
     HEROES << self
   end
 
