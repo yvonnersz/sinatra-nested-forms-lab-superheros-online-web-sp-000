c34d518ba8b4f0d48f35ef5d2f751e447bb33884
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-28 01:36:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :'views/super_hero'
+      erb :'PP/views/super_hero'
     end
 
 end
